9f77937a2d2180281b270d072b02f67a53c974a2
LuizVit0r <43145878+LuizVit0r@users.noreply.github.com>
2022-02-15 17:07:32 -0300

    Update and rename RectApp.java to Rect3DApp.java

--- 03-Rect3D/Rect3DApp.java
+++ 03-Rect3D/Rect3DApp.java
@@ -1,15 +1,15 @@
-public class RectApp {
+public class Rect3DApp {
     public static void main (String[] args) {
-        Rect r1 = new Rect(1,1,10,10,10,20,20); 
+        Rect3D r1 = new Rect3D(1,1,10,10,10,20,20); 
         r1.print();
      }
 } 
 
-class Rect {
+class Rect3D {
      int x, y;              // posiçao do retangulo
      int w, h, l;           // largura, altura e comprimento
      int xf, yf;            // o ponto de fuga do retangulo 3D
-     Rect (int x, int y, int w, int h, int l, int xf, int yf) {
+     Rect3D (int x, int y, int w, int h, int l, int xf, int yf) {
          this.x = x;
          this.y = y;
          this.w = w;
